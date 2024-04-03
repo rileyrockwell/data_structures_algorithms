3e2fa4a8d16550373181a9a32d2d233c38b6e2f2
Riley Rockwell <rileyrockwell@gmail.com>
2024-04-03 02:46:06 +0000

    updates

--- grind75/w01_arrays/1_twoSum.py
+++ grind75/w01_arrays/1_twoSum.py
@@ -1,4 +1,4 @@
-# topics: ...
+# topics: array, hashtable
 
 from typing import *
 
@@ -6,14 +6,20 @@ def twoSum(arr: List[int], target: int) -> List[int]:
 
 	index_dict = {}
 	
+	# assign index values to the respective integers in "arr"
 	for index, number in enumerate(arr):
 		
+		# determine the complement (to be used in following conditional)
 		complement = target - number
 
+		# if the complement exists, then the indexed valued associated with
+		# the iteration, becomes the answer
 		if complement in index_dict:
 			
+			# return the complement and index (obtained from the same iterative cycle)
 			return [index_dict[complement], index]
 
+		# if the complement does not exist, assign it to the dictionary.
 		index_dict[number] = index
 		
 
--- grind75/w01_arrays/3_majorityElement.py
+++ grind75/w01_arrays/3_majorityElement.py
@@ -4,15 +4,20 @@ from typing import *
 
 def majorityElement(nums: List[int]) -> int:
     
+    # count the number of occureneces of an integer in "nums"
     freq_dict = {integer: nums.count(integer) for integer in nums}
 
-    max_val = max(freq_dict.values())
+    # generate the same dictionary as before, with keys and values swaped
+    swaped_freq_dict = {nums.count(integer): integer for integer in nums}
+    
+    # determine the maximum value of all frequencies
+    max_freq = max(freq_dict.values())
 
-    return max_val
-
+    # call the original integer associated with the maxium value of all frequencies
+    return swaped_freq_dict[max_freq]
 
 
 # given: there will always exist a majority element
 nums = [0, 1, 2, 1, 1]
-
+nums = [3, 2, 3]
 print(majorityElement(nums))
