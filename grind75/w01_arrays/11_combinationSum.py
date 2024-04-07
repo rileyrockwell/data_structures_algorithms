69f132a981ae16b89ff1855fed832e7543d100e5
Riley Rockwell <rileyrockwell@gmail.com>
2024-04-07 19:09:40 +0000

    updates

---
+++ grind75/w01_arrays/11_combinationSum.py
@@ -0,0 +1,4 @@
+from typing import *
+
+def combinationSum(candidates: List[int], target: int) -> List[List[int]]:
+    pass
---
+++ grind75/w01_arrays/9_threeSum.py
@@ -0,0 +1,14 @@
+from typing import *
+
+def threeSum(nums: List[int]) -> List[List[int]]:
+    return -1
+
+
+
+
+
+nums = [-1,0,1,2,-1,-4]
+print(threeSum(nums))
+
+nums = [0,1,1]
+print(threeSum(nums))
