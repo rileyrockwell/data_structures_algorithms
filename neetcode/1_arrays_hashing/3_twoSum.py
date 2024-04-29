51da6f3f73aa6fc5f58b6371d0beafdbddf459ec
Riley Rockwell <rileyrockwell@gmail.com>
2024-04-28 21:52:46 -0700

    1. arrays & hashing

--- neetcode/1_arrays_hashing/3_twoSum.py
+++ neetcode/1_arrays_hashing/3_twoSum.py
@@ -3,22 +3,16 @@
 from typing import List
 
 def twoSum(nums: List[int], target: int) -> List[int]:
-    # arrays & hashing...in less than O(n^2) time
-    
-    # use a hashmap (dictionary)
-
-    # 1. map each element to its respective index
-    # 2. find the complement and determine if the complement equals target
-
-    # map each element of nums to its respective index
-    index_dict = {integer: index for index, integer in enumerate(nums)}
+    index_dict = {}
 
     for index in range(len(nums)):
         complement = target - nums[index]
 
-        if complement in nums:
-            return [index, index_dict[complement]]
+        if complement in index_dict:
+            return [index_dict[complement], index]
+        index_dict[nums[index]] = index
 
+    return []
     
         
     
@@ -30,3 +24,25 @@ print(twoSum(nums, target))
 nums = [6, 4, 2, 0]
 target = 10
 print(twoSum(nums, target))
+
+nums = [3, 2, 4]
+target = 6
+print(twoSum(nums, target))
+
+nums = [3, 0, 3]
+target = 6
+print(twoSum(nums, target))
+
+"""
+time: O(n)
+
+The for loop iterates over each element in the nums array once. This loop has a time complexity of O(n), where n is the length of the nums array.
+Inside the loop, each operation (comparing, accessing dictionary elements, and updating dictionary elements) is constant time, denoted as O(1).
+Therefore, the overall time complexity of the code is O(n). This is because the dominant factor in determining the time complexity is the iteration over the nums array, which occurs once.
+
+space: O(n)
+
+The index_dict dictionary is used to store elements from the nums array. In the worst case, the dictionary could contain all the elements of the nums array. Therefore, the space required for index_dict is proportional to the size of the nums array, giving it a space complexity of O(n).
+Apart from the index_dict, there are no other data structures whose space requirements depend on the size of the input.
+Therefore, the overall space complexity of the code is O(n), where n is the number of elements in the nums array.
+"""
---
+++ neetcode/1_arrays_hashing/4_groupAnagrams.py
@@ -0,0 +1,9 @@
+# 1. arrays & hashing
+
+from typing import List
+
+def groupAnagrams(strings: List[str]) -> List[List[str]]:
+    pass
+
+strings = ["eat","tea","tan","ate","nat","bat"]
+print(groupAnagrams(strings))
