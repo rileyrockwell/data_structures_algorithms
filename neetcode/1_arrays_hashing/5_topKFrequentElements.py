b3d932bea4ea66f3fea30d3d9818de7f69b0cf77
Riley Rockwell <rileyrockwell@gmail.com>
2024-04-28 23:12:36 -0700

    updates

--- neetcode/1_arrays_hashing/5_topKFrequentElements.py
+++ neetcode/1_arrays_hashing/5_topKFrequentElements.py
@@ -2,8 +2,15 @@
 
 from typing import List
 
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        pass
+def topKFrequent(nums: List[int], k: int) -> List[int]:
+    # time: better than O(n * log(n))
+    
 
 
-# generate your own examples (exhibit understanding)
+nums = [0, 1, 1, 2, 2, 2, 3, 3, 3, 3]
+k = 1
+print(topKFrequent(nums, k))
+
+nums = [0, 1, 1, 2, 2, 2, 3, 3, 3, 3]
+k = 2
+print(topKFrequent(nums, k))
