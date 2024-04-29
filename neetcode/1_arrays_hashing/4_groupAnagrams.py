b27942bd537a025c99149a23dc606b9ba28e685f
Riley Rockwell <rileyrockwell@gmail.com>
2024-04-28 22:54:58 -0700

    updates

--- neetcode/1_arrays_hashing/4_groupAnagrams.py
+++ neetcode/1_arrays_hashing/4_groupAnagrams.py
@@ -3,7 +3,34 @@
 from typing import List
 
 def groupAnagrams(strings: List[str]) -> List[List[str]]:
-    pass
+    # arrays & hashing
+    anagrams = {}
+
+    for string in strings:
+        key = "".join(sorted(string))
+        
+        # print(key, string)
+
+        if key not in anagrams:
+            anagrams[key] = []
+            anagrams[key].append(string)
+        else:
+            anagrams[key].append(string)
+
+    return list(anagrams.values())
+
 
 strings = ["eat","tea","tan","ate","nat","bat"]
 print(groupAnagrams(strings))
+
+strings = [""]
+print(groupAnagrams(strings))
+
+strings = ["a"]
+print(groupAnagrams(strings))
+
+"""
+time: ...
+
+space: ...
+"""
